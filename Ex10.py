659f5521b83266a637bc2ffc7069d627de934b65
An Dao Quoc <andq.2510012@usth.edu.vn>
2026-09-25 08:12:39 +0700

    Delete Ex10.py

--- Ex10.py
+++
@@ -1,9 +0,0 @@
-x = (int(input()))
-def divisors(n):
-    divisors = []
-    for i in range (1, n+1):
-        if n%i == 0:
-            divisors.append(i)
-    return divisors
-
-print(divisors(x))
